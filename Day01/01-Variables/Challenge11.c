58b4a22a19bac1eee89c0646e1dcd5417f9884ee
YassineElHassani <yassine.ds.404@gmail.com>
2024-09-24 20:05:46 +0200

    sas

---
+++ Day01/01-Variables/Challenge11.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int length, width;
+
+    printf("Enter the length: ");
+    scanf("%d", &length);
+    printf("Enter the width: ");
+    scanf("%d", &width);
+
+    int surface = length * width;
+    printf("Formula for the area of a rectangle: %d", surface);
+    
+    return 0;
+}
