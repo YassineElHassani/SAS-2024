5e1f14ccb07701b9fe51976f11852004ae0ce67d
YassineElHassani <yassine.ds.404@gmail.com>
2024-09-24 19:57:14 +0200

    sas

--- Day01/01-Variables/Challenge10.c
+++ Day01/01-Variables/Challenge10.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
+#include <math.h>
+
+
 
 int main() {
+    float r = 3.14159;
+    int radius;
+
+    printf("Enter sephre: ");
+    scanf("%d", &radius);
+
+    float volume = (4/3) * r * pow(radius, 3);
+    printf("Volume of a sphere: %d", volume);
     
+    return 0;
 }
