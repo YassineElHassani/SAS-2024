6851c7af30293a026758017767f2b6f8ec917c9c
YassineElHassani <yassine.ds.404@gmail.com>
2024-09-24 15:03:52 +0200

    sas

--- Day01/Challenge1.c
+++ Day01/Challenge1.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
 
 int main() {
+    int age;
     char fname[10];
     char lname[10];
     char gender[6];
     char email[20];
-    int age;
 
     printf("Enter Your First Name: ");
     scanf("%s", &fname);
     printf("Enter Your Last Name: ");
     scanf("%s", &lname);
-    printf("Enter Your Gender: ");
-    scanf("%s", &gender);
     printf("Enter Your Age: ");
     scanf("%d", &age);
+    printf("Enter Your Gender: ");
+    scanf("%s", &gender);
     printf("Enter Your Email: ");
-
-    Printf("Your First Name: %s\nYour Last Name: %s\nYour Age: %d\nYour Gender: %s\nYour Email: %s\n");
+    scanf("%s", &email);
+    printf("\n");
+    printf("Your First Name: %s \nYour Last Name: %s \nYour Age: %d \nYour Gender: %s \nYour Email: %s \n", fname,lname,age,gender,email);
 }
