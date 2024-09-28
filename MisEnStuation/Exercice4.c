3ee5d1b502f8ab85ac60b206747db2f3d8c21c3d
YassineElHassani <yassine.ds.404@gmail.com>
2024-09-28 13:39:33 +0100

    sas

--- MisEnStuation/Exercice4.c
+++ MisEnStuation/Exercice4.c
@@ -1,16 +1,25 @@
 #include <stdio.h>
 
 void main() {
-    float temperatures[5];
-    
+    float arr[5];
+    float min, max;
 
-    printf("Entree: ");
-    scanf("%d", &temperatures);
-
-    for (int i = 1; i < 5; i++) {
-        //float temperatures -= i;
+    printf("Enter 5 numbers: ");
+    for(int i = 0; i < 5; i++) {
+        scanf("%f", &arr[i]);
     }
 
-    //No Edea !!
+    min = max = arr[0];
 
+    for(int i = 1; i < 5; i++) {
+        if(arr[i] < min) {
+            min = arr[i];
+        }
+        if(arr[i] > max) {
+            max = arr[i];
+        }
+    }
+
+    printf("Minimum: %.2f\n", min);
+    printf("Maximum: %.2f\n", max); 
 }
