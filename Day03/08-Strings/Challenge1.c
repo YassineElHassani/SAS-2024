ce0686278a78a9aff64c42c27febf42d1b134152
YassineElHassani <yassine.ds.404@gmail.com>
2024-09-30 16:31:33 +0100

    sas

--- Day03/08-Strings/Challenge1.c
+++ Day03/08-Strings/Challenge1.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
 void main() {
-    
+    char character[30];
+
+    printf("Enter a character: ");
+    scanf("%s", &character);
+
+    printf("The character is: %s\n", character);
 }
