8d957e70f7fb1c116cc7a0b705090582dffa1b68
YassineElHassani <yassine.ds.404@gmail.com>
2024-10-01 23:09:58 +0100

    mini-project

--- Day03/mini-project/project.c
+++ Day03/mini-project/project.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-#include <strings.h>
+#include <string.h>
 
 struct Contact {
     char name[20];
-    char number[10];
+    char number[20];
     char email[50];
 };
 
@@ -14,7 +14,7 @@ void addContact() {
     if(size < 100) {
         printf("==> Enter the name: ");
         scanf("%s", conTab[size].name);
-        printf("==> Enter the number phone: ");
+        printf("==> Enter the phone number: ");
         scanf("%s", conTab[size].number);
         printf("==> Enter the email: ");
         scanf("%s", conTab[size].email);
@@ -24,41 +24,42 @@ void addContact() {
         printf("------------------------------------------------------------------------------\n");
         printf("|        Phone number       |  %s\n", conTab[size].number);
         printf("------------------------------------------------------------------------------\n");
-        printf("|           Emai            |  %s\n", conTab[size].email);
+        printf("|           Email           |  %s\n", conTab[size].email);
         printf("------------------------------------------------------------------------------\n");
         printf("\n");
         size++;
     } else {
-        printf("Stockage full!!\n");
+        printf("Storage full!!\n");
     }
 }
 
 void editContact() {
-    char search[20], new_number, new_email;
+    char search[20], new_number[20], new_email[50];
     int found = 0;
 
     printf("==> Enter the contact name you want to edit: ");
-    scanf(" %[^\n\s+]", search);
+    scanf(" %[^\n]", search);
     
     for(int i = 0; i < size; i++) {
         if(strcasecmp(conTab[i].name, search) == 0) {
             found = 1;
-            printf("Current phone number of '%s': %d\n", conTab[i].name, conTab[i].number);
-            printf("==> Enter the new number: ");
-            scanf(" %[^\n\s+]", new_number);
-            strcpy(conTab[i].number, new_number);
-            printf("The number has been updated\n");
             
-            printf("Current email of '%s': %s\n", conTab[i].name, conTab[i].email);
+            printf("==> Enter the new number: ");
+            scanf("%s", new_number);
+            strcpy(conTab[i].number, new_number);
+
+            printf("The number has been updated.\n");
+
             printf("==> Enter the new email: ");
-            scanf(" %[^\n\s+]", new_email);
+            scanf("%s", new_email);
             strcpy(conTab[i].email, new_email);
-            printf("The email has been updated\n");
+
+            printf("The email has been updated.\n");
             break;
         }
     }
     if (!found) {
-        printf("The contact is not available\n");
+        printf("The contact is not available.\n");
     }
 }
 
@@ -67,32 +68,35 @@ void deleteContact() {
     int found = 0;
 
     printf("==> Enter the name of the contact you want to remove: ");
-    scanf(" %[^\n\s+]", search);
+    scanf(" %[^\n]", search);
 
     for(int i = 0; i < size; i++) {
         if(strcasecmp(conTab[i].name, search) == 0) {
             found = 1;
-            printf("The contact has been found %s\n", conTab[i].name);
+            printf("The contact has been found: %s\n", conTab[i].name);
             for(int j = i; j < size - 1; j++) {
-                conTab[i] = conTab[i + 1];
+                conTab[j] = conTab[j + 1];
             }
             size--;
-            printf("The contact has been removed.\n");
+            printf("The contact has been removed.\n\n");
             break;
         }
+    }
+    if (!found) {
+        printf("Contact not found.\n");
     }
 }
 
 void showContact() {
     if(size == 0) {
-        printf("No contact are avaliable!\n");
+        printf("No contacts are available!\n\n");
         return;
     }
     printf("------------------------------------------------------------------------------\n");
     printf("|          Name           |      Phone number      |          Email          |\n");
     printf("------------------------------------------------------------------------------\n");
     for(int i = 0; i < size; i++) {
-        printf("|   %s   |   %s   |   %s   |", conTab[i].name, conTab[i].number, conTab[i].email);
+        printf("|   %-19s   |   %-18s   | %-5s |\n", conTab[i].name, conTab[i].number, conTab[i].email);
     }
     printf("------------------------------------------------------------------------------\n");
 }
@@ -101,8 +105,8 @@ void searchContact() {
     char search[20];
     int found = 0;
 
-    printf("Enter the name of contact you searching for: ");
-    scanf(" %[^\n\s+]", search);
+    printf("==> Enter the name of the contact you're searching for: ");
+    scanf(" %[^\n]", search);
 
     for(int i = 0; i < size; i++) {
         if(strcasecmp(conTab[i].name, search) == 0) {
@@ -110,26 +114,28 @@ void searchContact() {
             printf("------------------------------------------------------------------------------\n");
             printf("|          Name           |      Phone number      |          Email          |\n");
             printf("------------------------------------------------------------------------------\n");
-            printf("|   %s   |   %s   |   %s   |", conTab[i].name, conTab[i].number, conTab[i].email);
+            printf("|   %-20s   |   %-15s   |   %-30s   |\n", conTab[i].name, conTab[i].number, conTab[i].email);
             printf("------------------------------------------------------------------------------\n");
             break;
         }
     }
+    if (!found) {
+        printf("Contact not found.\n\n");
+    }
 }
 
-void main() {
+int main() {
     int choise;
 
     while (1) {
         printf("- 1 - Add a Contact\n");
         printf("- 2 - Edit a Contact\n");
         printf("- 3 - Delete a Contact\n");
-        printf("- 4 - Show All conTab\n");
+        printf("- 4 - Show All Contacts\n");
         printf("- 5 - Search for a Contact\n");
-        printf("- 6 - Exite\n");
-        printf("\n");
-        printf("==> Enter your choise: ");
-        scanf("%d", choise);
+        printf("- 6 - Exit\n\n");
+        printf("==> Enter your choice: ");
+        scanf("%d", &choise);
 
         switch (choise) {
         case 1:
@@ -148,11 +154,10 @@ void main() {
             searchContact();
             break;
         case 6:
-            printf("Exiting the program.\n");
-            return;
-            break;      
+            printf("Exiting the program.\n\n");
+            return 0;
         default:
-            printf("Please try again.\n");
+            printf("Invalid choice! Please try again.\n\n");
             break;
         }
     }
