8365259adfba74cbda8ca108cb7d089c78cfcb96
YassineElHassani <yassine.ds.404@gmail.com>
2024-10-03 23:22:46 +0100

    Project_Fin_SAS :)

--- Project_Fin_SAS/project.c
+++ Project_Fin_SAS/project.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#define DEFAULT   "\033[0m"
+#define CYAN    "\033[36m"
+#define RED     "\033[31m"
+#define GREEN   "\033[32m"
+#define YELLOW  "\033[33m"
+#define BLUE    "\033[34m"
+
 
 struct Date {
     int day;
@@ -21,9 +29,17 @@ struct Info info[100];
 int size = 0;
 int uniqueCounter = 0;
 
+void clearScreen() {
+    #ifdef _WIN32
+        system("cls");
+    #else
+        system("clear");
+    #endif
+}
+
 void addReservation() {
     if(size < 100) {
-        printf("==> Enter First Name: ");
+        printf("%s==> Enter First Name: ", YELLOW);
         getchar();
         fgets(info[size].fName, sizeof(info[size].fName), stdin);
         info[size].fName[strcspn(info[size].fName, "\n")] = '\0';
@@ -31,40 +47,40 @@ void addReservation() {
         printf("==> Enter Last Name: ");
         fgets(info[size].lName, sizeof(info[size].lName), stdin);
         info[size].lName[strcspn(info[size].lName, "\n")] = '\0';
-        
 
         printf("==> Enter Phone Number: ");
-        fflush(stdin);
         scanf("%d", &info[size].number);
 
         printf("==> Enter Age: ");
-        fflush(stdin);
         scanf("%d", &info[size].age);
-
-        printf("==> Enter Status(valid, postpon, cancel, process): ");
+        
+        printf("==> Enter Status(valid, postpone, cancel, process): ");
         getchar();
         fgets(info[size].status, sizeof(info[size].status), stdin);
         info[size].status[strcspn(info[size].status, "\n")] = '\0';
 
         printf("==> Enter reservation Date(DD MM YYYY): ");
+        printf("%s", DEFAULT);
         fflush(stdin);
         scanf("%d %d %d", &info[size].date.day, &info[size].date.month, &info[size].date.year);
 
         info[size].uniqueRef = uniqueCounter++;
         
-        printf("\nReservation added successfully!\n\n");
+        printf("\n%sReservation added successfully!%s\n\n", GREEN, DEFAULT);
         size++;
     } else {
-        printf("Storage full cannot add more reservations.\n\n");
+        printf("%sStorage full cannot add more reservations.%s\n\n", RED, DEFAULT);
     }
+    printf("Press enter to continue");
+    getchar();
+    getchar();
 }
-
 
 void reservationSettings() {
     int searchReferance;
     int found = 0;
 
-    printf("==> Enter the referance(ID) of the reservation you want to modify or delete: ");
+    printf("%s==> Enter the referance(ID) of the reservation you want to modify or delete: %s", YELLOW, DEFAULT);
     scanf("%d", &searchReferance);
 
     for (int i = 0; i < size; i++) {
@@ -72,9 +88,9 @@ void reservationSettings() {
             found = 1;
             int choice;
 
-            printf("Reservation found. Choose an action:\n");
+            printf("%sReservation found. Choose an action:%s\n", GREEN, DEFAULT);
             printf("- 1 - Modify\n- 2 - Delete\n\n");
-            printf("==> Enter choice: ");
+            printf("%s==> Enter choice: ", YELLOW);
             scanf("%d", &choice);
 
             if (choice == 1) {
@@ -82,36 +98,38 @@ void reservationSettings() {
                 scanf("%d", &info[i].number);
                 printf("==> Enter new Age: ");
                 scanf("%d", &info[i].age);
-                printf("==> Enter new Status(valid, postpon, cancel, process): ");
+                printf("==> Enter new Status(valid, postpone, cancel, process): ");
                 scanf("%s", info[i].status);
                 printf("==> Enter new reservation Date(DD MM YYYY): ");
+                printf("%s", DEFAULT);
                 scanf("%d %d %d", &info[i].date.day, &info[i].date.month, &info[i].date.year);
-                printf("Reservation updated successfully!\n\n");
+                printf("%sReservation updated successfully!%s\n\n", GREEN, DEFAULT);
             } else if (choice == 2) {
                 for (int j = i; j < size - 1; j++) {
                     info[j] = info[j + 1];
                 }
                 size--;
-                printf("Reservation deleted successfully!\n");
+                printf("%sReservation deleted successfully!%s\n", GREEN, DEFAULT);
             }
             break;
         }
     }
 
     if (!found) {
-        printf("No reservation found with that name.\n\n");
+        printf("%sNo reservation found with that name.%s\n\n", RED, DEFAULT);
     }
+    printf("Press enter to continue");
+    getchar();
+    getchar();
 }
-
-
 
 void showReservation() {
     if (size == 0) {
-        printf("No reservations available!\n");
+        printf("%sNo reservations available!%s\n\n", RED, DEFAULT);
         return;
     }
 
-    printf("---------------------------------------------------------------------------------------------------\n");
+    printf("%s---------------------------------------------------------------------------------------------------\n", CYAN);
     printf("|   ID   |   First Name    |    Last Name    |    Phone    | Age |    Status     |      Date      |\n");
     printf("---------------------------------------------------------------------------------------------------\n");
 
@@ -121,22 +139,25 @@ void showReservation() {
                info[i].status, info[i].date.day, info[i].date.month, info[i].date.year);
     }
 
-    printf("---------------------------------------------------------------------------------------------------\n\n");
+    printf("---------------------------------------------------------------------------------------------------%s\n\n", DEFAULT);
+    printf("Press enter to continue");
+    getchar();
+    getchar();
 }
 
 void reservationTri() {
     int choice;
-    printf("Choose type of sorting:\n");
+    printf("\nChoose type of sorting:\n");
     printf("- 1 - Sort by name\n- 2 - Sort by status\n\n");
-    printf("==> Enter your choice: ");
-    scanf("%d", choice);
+    printf("%s==> Enter your choice: %s", YELLOW, DEFAULT);
+    scanf("%d", &choice);
 
     if(choice == 1) {
-        printf("-----------------------------------------------------------\n");
-        printf("|                    Sort reservations                    |\n");
-        printf("-----------------------------------------------------------\n");
-        printf("|         Last Name           |           Status          |\n");
-        printf("-----------------------------------------------------------\n");
+        printf("%s-------------------------------------------------------------\n", CYAN);
+        printf("|                     Sort reservations                     |\n");
+        printf("-------------------------------------------------------------\n");
+        printf("|         Last Name           |            Status           |\n");
+        printf("-------------------------------------------------------------\n");
 
         for(int i = 0; i < size; i++) {
         char sort[15];
@@ -147,16 +168,16 @@ void reservationTri() {
                 strcpy(info[j].lName, sort);
             }
         }
-        printf("|  %-23s |  %-10s |\n", info[i].lName, info[i].status);
+        printf("|  %-26s |  %-26s |\n", info[i].lName, info[i].status);
         }
 
-        printf("-----------------------------------------------------------\n");
+        printf("-------------------------------------------------------------\n");
     } else if(choice == 2) {
-        printf("-----------------------------------------------------------\n");
-        printf("|                    Sort reservations                    |\n");
-        printf("-----------------------------------------------------------\n");
-        printf("|          Status          |           Last Name          |\n");
-        printf("-----------------------------------------------------------\n");
+        printf("-------------------------------------------------------------\n");
+        printf("|                     Sort reservations                     |\n");
+        printf("-------------------------------------------------------------\n");
+        printf("|           Status            |          Last Name          |\n");
+        printf("-------------------------------------------------------------\n");
 
         for(int i = 0; i < size; i++) {
         char sort[15];
@@ -167,16 +188,16 @@ void reservationTri() {
                 strcpy(info[j].status, sort);
             }
         }
-        printf("|  %-23s |  %-23s |\n", info[i].status, info[i].lName);
+        printf("|  %-26s |  %-26s |\n", info[i].status, info[i].lName);
         }
 
-        printf("-----------------------------------------------------------\n");
+        printf("-----------------------------------------------------------%s\n", DEFAULT);
     }
     printf("\n\n");
+    printf("Press enter to continue");
+    getchar();
+    getchar();
 }
-
-
-
 
 void searchReservation() {
     char searchName[15];
@@ -185,120 +206,176 @@ void searchReservation() {
 
     printf("Choose type of search:\n");
     printf("- 1 - By last name\n- 2 - By referance(ID)\n\n");
-    printf("==> Enter choice: ");
+    printf("%s==> Enter choice: ", YELLOW);
     scanf("%d", &choice);
 
     if(choice == 1) {
-        printf("==> Enter the last name of the reservation you are searching for: ");
+        printf("==> Enter the last name of the reservation you are searching for: %s", DEFAULT);
         scanf(" %[^\n]", searchName);
 
         for (int i = 0; i < size; i++) {
         if (strcasecmp(info[i].lName, searchName) == 0) {
             found = 1;
-            printf("\nReservation found!\n\n");
+            printf("\n%sReservation found!%s\n\n", GREEN, DEFAULT);
 
-            printf("First Name: %s\n", info[i].fName);
+            printf("%sFirst Name: %s\n", BLUE, info[i].fName);
             printf("Last Name: %s\n", info[i].lName);
             printf("Phone Number: %d\n", info[i].number);
             printf("Age: %d\n", info[i].age);
             printf("Status: %s\n", info[i].status);
-            printf("Reservation Date: %02d/%02d/%04d\n\n", info[i].date.day, info[i].date.month, info[i].date.year);
+            printf("Reservation Date: %02d/%02d/%04d %s\n\n", info[i].date.day, info[i].date.month, info[i].date.year, DEFAULT);
             break;
         }
     }
     } else if(choice == 2) {
-        printf("==> Enter the ID of the reservation you are searching for: ");
+        printf("%s==> Enter the ID of the reservation you are searching for: %s", YELLOW, DEFAULT);
         scanf("%d", &searchReferance);
 
         for(int i = 0; i < size; i++) {
             if(info[size].uniqueRef == searchReferance) {
                 found = 1;
-                printf("\nReservation found!\n\n");
+                printf("\n%sReservation found!%s\n\n", GREEN, DEFAULT);
 
-                printf("First Name: %s\n", info[i].fName);
+                printf("%sFirst Name: %s\n", BLUE, info[i].fName);
                 printf("Last Name: %s\n", info[i].lName);
                 printf("Phone Number: %d\n", info[i].number);
                 printf("Age: %d\n", info[i].age);
                 printf("Status: %s\n", info[i].status);
-                printf("Reservation Date: %02d/%02d/%04d\n\n", info[i].date.day, info[i].date.month, info[i].date.year);
+                printf("Reservation Date: %02d/%02d/%04d %s\n\n", info[i].date.day, info[i].date.month, info[i].date.year, DEFAULT);
                 break;
             }
         }
     }
 
     if (!found) {
-        printf("\nNo reservation found with that name.\n\n");
-    } 
+        printf("\n%sNo reservation found!%s\n\n", RED, DEFAULT);
+    }
+    printf("Press enter to continue");
+    getchar();
+    getchar();
 }
 
-
 void statistic() {
-    int totalAge = 0, valid = 0, cancel = 0, postpon = 0, process = 0;
+    int totalAge = 0, valid = 0, cancel = 0, postpone = 0, process = 0;
+    int ageGroups[3] = {0};
 
     for (int i = 0; i < size; i++) {
         totalAge += info[i].age;
+
+        if (info[i].age <= 18) {
+            ageGroups[0]++;
+        } else if (info[i].age <= 35) {
+            ageGroups[1]++;
+        } else {
+            ageGroups[2]++;
+        }
 
         if(strcmp(info[i].status, "valid") == 0) {
             valid++;
         } else if(strcmp(info[i].status, "cancel") == 0) {
             cancel++;
-        } else if(strcmp(info[i].status, "postpon") == 0) {
-            postpon++;
+        } else if(strcmp(info[i].status, "postpone") == 0) {
+            postpone++;
         } else if(strcmp(info[i].status, "process") == 0) {
             process++;
         }
     }
 
     if (size > 0) {
-    printf("\nAverage age: %.2f\n", (float)totalAge / size);
+        printf("\n%sAverage age: %.2f\n\n", CYAN, (float)totalAge / size);
     }
 
-    printf("Total validated: %d\n", valid);
-    printf("Total canceled: %d\n", cancel);
-    printf("Total postponed: %d\n", postpon);
-    printf("Total processed: %d\n\n", process);
-}
+    printf("-----------------------------------------------------------\n");
+    printf("|                Reservation Statistics                   |\n");
+    printf("-----------------------------------------------------------\n");
+    printf("| Age Group                |     Count                    |\n");
+    printf("-----------------------------------------------------------\n");
+    printf("| 0-18 years old           |     %-24d |\n", ageGroups[0]);
+    printf("| 19-35 years old          |     %-24d |\n", ageGroups[1]);
+    printf("| 36+ years old            |     %-24d |\n", ageGroups[2]);
+    printf("-----------------------------------------------------------\n\n");
 
+    printf("-----------------------------------------------------------\n");
+    printf("| Reservation Status       |     Count                    |\n");
+    printf("-----------------------------------------------------------\n");
+    printf("| Valid                    |     %-24d |\n", valid);
+    printf("| Canceled                 |     %-24d |\n", cancel);
+    printf("| postponeed                |     %-24d |\n", postpone);
+    printf("| In Process               |     %-24d |\n", process);
+    printf("-----------------------------------------------------------%s\n\n", DEFAULT);
+    printf("Press enter to continue");
+    getchar();
+    getchar();
+}
 
 void main() {
     int choice;
 
-    while(1) {
-        printf("- 1 - Add a reservation\n");
-        printf("- 2 - Modify or delete a reservation\n");
-        printf("- 3 - View reservation details\n");
-        printf("- 4 - Sorting reservations\n");
-        printf("- 5 - Search for reservations\n");
-        printf("- 6 - Statistics\n");
-        printf("- 7 - Exit\n\n");
+    //info[0] = (struct Info){{04, 10, 2024}, "yassine", "el marnissi", "valid", 678987654, 25, 91};
+    //info[1] = (struct Info){{25, 04, 2024}, "amine", "choon", "valid", 623845961, 12, 92};
+    //info[2] = (struct Info){{15, 02, 2024}, "reda", "monir", "valid", 674265812, 7, 93};
+    //info[3] = (struct Info){{22, 11, 2024}, "hind", "soufi", "cancel", 754963225, 36, 94};
+    //info[4] = (struct Info){{23, 07, 2024}, "monir", "rachaad", "postpone", 664825576, 27, 95};
+    //info[5] = (struct Info){{12, 12, 2024}, "soumia", "anib", "postpone", 714852354, 64, 96};
+    //info[6] = (struct Info){{18, 03, 2024}, "rachida", "ben kader", "process", 698456758, 38, 97};
+    //info[7] = (struct Info){{19, 07, 2024}, "hamid", "al mouhamadi", "cancel", 695135785, 46, 98};
+    //info[8] = (struct Info){{07, 11, 2024}, "kholoud", "zanir", "process", 636145879, 43, 99};
+    //info[9] = (struct Info){{16, 05, 2024}, "zinb", "idrissi", "valid", 696584236, 17, 100};
 
-        printf("==> Enter your choice: ");
+
+    while(1) {
+
+        printf("%s             ___   ____  _     _____   __    _                   \n", YELLOW);
+        printf("            | | \\ | |_  | |\\ |  | |   / /\\  | |                  \n");
+        printf("            |_|_/ |_|__ |_| \\|  |_|  /_/--\\ |_|__                \n");
+        printf(" ___   ____  __   ____  ___   _       __   _____  _   ___   _    \n");
+        printf("| |_) | |_  ( (` | |_  | |_) \\ \\  /  / /\\   | |  | | / / \\ | |\\ |\n");
+        printf("|_| \\ |_|__ _)_) |_|__ |_| \\  \\_\\/  /_/--\\  |_|  |_| \\_\\_/ |_| \\|%s\n\n", DEFAULT);
+        printf("%s--------------------------------------------------------------------\n", CYAN);
+        printf("|                        Reservation Menu                          |\n");
+        printf("--------------------------------------------------------------------\n");
+        printf("| 1 | Add a reservation                                            |\n");
+        printf("| 2 | Modify or delete a reservation                               |\n");
+        printf("| 3 | View reservation details                                     |\n");
+        printf("| 4 | Sorting reservations                                         |\n");
+        printf("| 5 | Search for reservations                                      |\n");
+        printf("| 6 | Statistics                                                   |\n");
+        printf("| 7 | Exit                                                         |\n");
+        printf("--------------------------------------------------------------------%s\n", DEFAULT);
+
+        printf("%s==> Enter your choice: %s", YELLOW, DEFAULT);
         scanf("%d", &choice);
 
         switch(choice) {
             case 1:
                 addReservation();
+                clearScreen();
                 break;
             case 2:
                 reservationSettings();
+                clearScreen();
                 break;
             case 3:
                 showReservation();
+                clearScreen();
                 break;
             case 4:
                 reservationTri();
+                clearScreen();
                 break;
             case 5:
                 searchReservation();
+                clearScreen();
                 break;
             case 6:
                 statistic();
+                clearScreen();
                 break;
             case 7:
-                printf("Exiting the program.\n\n");
+                printf("%sExiting the program.%s\n\n", GREEN, DEFAULT);
                 return;
             default:
-                printf("Invalid choice! Please try again.\n\n");
+                printf("%sInvalid choice! Please try again.%s\n\n", RED, DEFAULT);
                 break;
         }
     }
